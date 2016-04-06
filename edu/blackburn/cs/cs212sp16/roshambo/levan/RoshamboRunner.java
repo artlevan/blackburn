b1f1503e8f68c0dd72523398870e8772d711b29e
arthur <arthur@LevanPC>
2016-04-06 14:41:14 -0500

    added dialog controller

--- edu/blackburn/cs/cs212sp16/roshambo/levan/RoshamboRunner.java
+++ edu/blackburn/cs/cs212sp16/roshambo/levan/RoshamboRunner.java
@@ -25,7 +25,7 @@ public class RoshamboRunner extends Application {
     @Override
     public void start(Stage primaryStage) throws IOException {
         Stage dialogStage = new Stage();
-        VBox vb = FXMLLoader.load(getClass().getResource("Dialog.fxml"));
+        Parent vb = FXMLLoader.load(getClass().getResource("Dialog.fxml"));
         Scene dialogScene = new Scene(vb);
         dialogStage.setScene(dialogScene);
         dialogStage.showAndWait();
