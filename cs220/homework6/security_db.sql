b1d255608bf56db2a54291eafd1d4670ca046833
artlevan <arthur.levan@blackburn.edu>
2016-10-10 17:03:05 +0000

    Updated security_db.sql

--- cs220/homework6/security_db.sql
+++ cs220/homework6/security_db.sql
@@ -2,3 +2,64 @@
 --Date: 10/7/16
 --Estimate: 5 hours
 --Actual: 
+
+drop table if exists student;
+drop table if exists studentworker;
+drop table if exists department;
+drop table if exists facility;
+drop table if exists procedure;
+drop table if exists incidentreport;
+
+create database security;
+
+create table student
+(
+studentid int primary key not null,
+firstname varchar(64) not null,
+lastname varchar(64) not null,
+studentproblem varchar(64)
+);
+
+create table studentworker
+(
+studentid int not null,
+workerid int not null,
+departmentid int not null,
+position varchar(64) not null,
+primary key (studentid, workerid, departmentid),
+foreign key (studentid) references student(studentid),
+foreign key (departmentid) references department(departmentid)
+);
+
+create table department
+(
+departmentid int not null,
+facilityid int not null,
+departmentname vachcar(64) not null,
+phonenumber char(10) not null,
+primary key (departmentid, facilityid),
+foreign key (facilityid) references facility(facilityid)
+);
+
+create table facility
+(
+facilityid int not null primary key,
+facilityname varchar(64) not null,
+);
+
+create table procedure
+(
+procedureid int not null primary key,
+procedurename varchar(64) not null,
+);
+
+create table incidentreport
+(
+incidentid int not null,
+workerid int not null,
+emergency bit not null,
+summary text not null,
+incidentdate date,
+primary key (incidentid, workerid),
+foreign key (workerid) references studentworker(workerid)
+);
