c5eb172fd90af11a64507f116a44bee50cca0820
artlevan <arthur.levan@blackburn.edu>
2016-10-21 20:29:12 +0000

    Updated Homework 7

--- cs220/homework7/cuboid.rb
+++ cs220/homework7/cuboid.rb
@@ -1,4 +1,49 @@
 --Arthur Levan
 --Estimated Time: 2 hours
 
+class Cuboid
+  attr_reader :height, :width, :depth
 
+    def initialize(height, width, depth)
+        @height = height
+        @width = width
+        @minwidth = 2
+	end
+
+    def calculate_area()
+        2*(@width*@height + @depth*@height + @width*@depth)
+    end
+
+    def calculate_volume()
+        @width*@height*@depth
+    end
+
+    def set_height(newheight)
+        if newheight > 0
+            puts "Can't make height 0"
+        else
+            @height = newheight
+        end
+    end
+
+    def set_width(newwidth)
+        if newwidth > 0
+            puts "Can't make width 0"
+        else 
+            @width = newwidth
+        end
+    end
+
+    def set_depth(newdepth)
+        if newdepth > 0
+            puts "Can't make depth 0"
+        else 
+            @depth = newdepth
+        end
+    end
+end
+
+if __FILE__ == $0
+    c = Cuboid.new(1,2,3)
+    puts "Width is #{c.width}"
+end
