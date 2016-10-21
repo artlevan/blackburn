21a6d8381d0310d96dccdb27d5b00cae64b47035
artlevan <arthur.levan@blackburn.edu>
2016-10-21 21:18:22 +0000

    added more code

--- cs220/homework7/cuboid.rb
+++ cs220/homework7/cuboid.rb
@@ -1,14 +1,14 @@
---Arthur Levan
---Estimated Time: 2 hours
+#Arthur Levan
+#Estimated Time Two Hours
 
 class Cuboid
-  attr_reader :height, :width, :depth
+    attr_reader :height, :width, :depth
 
     def initialize(height, width, depth)
         @height = height
         @width = width
-        @minwidth = 2
-	end
+        @depth = depth
+    end
 
     def calculate_area()
         2*(@width*@height + @depth*@height + @width*@depth)
@@ -18,24 +18,24 @@ class Cuboid
         @width*@height*@depth
     end
 
-    def set_height(newheight)
-        if newheight > 0
+    def height=(newheight)
+        if newheight == 0
             puts "Can't make height 0"
         else
             @height = newheight
         end
     end
 
-    def set_width(newwidth)
-        if newwidth > 0
+    def width=(newwidth)
+        if newwidth == 0
             puts "Can't make width 0"
         else 
             @width = newwidth
         end
     end
 
-    def set_depth(newdepth)
-        if newdepth > 0
+    def depth=(newdepth)
+        if newdepth == 0
             puts "Can't make depth 0"
         else 
             @depth = newdepth
@@ -45,5 +45,19 @@ end
 
 if __FILE__ == $0
     c = Cuboid.new(1,2,3)
-    puts "Width is #{c.width}"
+	puts "Height is #{c.height}"
+	puts "Width is #{c.width}"
+	puts "Depth is #{c.depth}"
+	print "The volume is "
+	puts c.calculate_volume()
+	print "The surface area is "
+	puts c.calculate_area()
+	print "Please select a field to change "
+	input = gets	
+	if input == "depth"
+		puts "Enter a new value for " + #{input}
+		value = gets
+		c.depth(value)
+	end
+	puts "End of File"
 end
