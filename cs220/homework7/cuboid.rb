732aada814657d84d16046d9e7eb989fce52aa70
artlevan <arthur.levan@blackburn.edu>
2016-10-22 19:12:57 +0000

    Added Loop

--- cs220/homework7/cuboid.rb
+++ cs220/homework7/cuboid.rb
@@ -18,7 +18,7 @@ class Cuboid
         @width*@height*@depth
     end
 
-    def height=(newheight)
+    def set_height(newheight)
         if newheight == 0
             puts "Can't make height 0"
         else
@@ -26,7 +26,7 @@ class Cuboid
         end
     end
 
-    def width=(newwidth)
+    def set_width(newwidth)
         if newwidth == 0
             puts "Can't make width 0"
         else 
@@ -34,30 +34,53 @@ class Cuboid
         end
     end
 
-    def depth=(newdepth)
+    def set_depth(newdepth)
         if newdepth == 0
             puts "Can't make depth 0"
         else 
             @depth = newdepth
         end
     end
-end
+
+    def numbers()
+	puts "Height is " + @height.to_s
+	puts "Width is " + @width.to_s
+	puts "depth is " + @depth.to_s
+	print "The Volume is "
+	puts calculate_volume()
+	print "The Area is "
+	puts calculate_area()
+    end
+
+    def input()
+	print "Please enter a Field to change "
+	field = gets.chomp.downcase
+	puts "Enter a new value for #{field}"
+	newvalue = gets.chomp
+	value = newvalue.to_i
+	if field == "depth"
+		set_depth(value)
+	elsif field == "height"
+		set_height(value)
+	elsif field == "width"
+		set_width(value)
+	end
+    end
+
+end	
 
 if __FILE__ == $0
     c = Cuboid.new(1,2,3)
-	puts "Height is #{c.height}"
-	puts "Width is #{c.width}"
-	puts "Depth is #{c.depth}"
-	print "The volume is "
-	puts c.calculate_volume()
-	print "The surface area is "
-	puts c.calculate_area()
-	print "Please select a field to change "
-	input = gets	
-	if input == "depth"
-		puts "Enter a new value for " + #{input}
-		value = gets
-		c.depth(value)
+	puts c.numbers
+	while
+	c.input()
+	puts "The new numbers are "
+	puts c.numbers()
+	puts "Would you like to change another field? (Y/N)"
+	input = gets.chomp.upcase
+		if input == "N"
+			break
+		end
 	end
-	puts "End of File"
 end
+
