91989e2ab5eb835298cccc91782bfffaf40e9ac7
arthur.levan <arthur.levan@HU130H001.Blackburn.College>
2017-02-10 11:41:48 -0600

    added Queue interface

---
+++ edu/blackburn/cs/cs313sp17/levan/src/queue/Queue.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.blackburn.cs.cs313sp17.levan.src.queue;
+
+/**
+ *
+ * @author arthur.levan
+ */
+public interface Queue<E> {
+    /*
+    Adds an item to the back of the queue
+    */
+    public void enqueue(E Item);
+    /*
+    Removes an item from the front of the queue
+    */
+    public void dequeue(E Item);
+    /*
+    Clears the queue of all items
+    */
+    public void clear();
+    /*
+    Returns the item at the front of the queue without removing it
+    */
+    public E front();
+    /*
+    Returns the current size of the Queue
+    */
+    public int size();
+    /*
+    Returns true if the Queue is currently full
+    */
+    public boolean isFull();
+    
+}
