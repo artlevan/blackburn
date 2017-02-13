298c6021bb46a92282c614fe5501e284329d7651
arthur.levan <arthur.levan@HU130C03.Blackburn.College>
2017-02-13 14:05:38 -0600

    Added some Binary test code

---
+++ Testcode/BinaryConversion.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Testcode;
+
+/**
+ *
+ * @author arthur.levan
+ */
+public class BinaryConversion {
+    
+    
+    public static void main(String[] args) {
+        String beta = "11011001";
+        int alpha = 432;
+       
+
+        System.out.println(Integer.toBinaryString(918241));
+    }
+}
